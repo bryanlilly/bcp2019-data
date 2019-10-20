32b43a1804ad2683ae24d3d2d31523922022cafa
Bryan <lilly.bryan@gmail.com>
2019-10-20 14:28:17 -0400

    feat: add Proper pivot table

---
+++ src/calendar/ProperTable.ts
@@ -0,0 +1,182 @@
+export const ProperTable = [
+    {
+        id: 'proper1',
+        name: 'Proper 1',
+        min: 508,
+        max: 514
+    },
+    {
+        id: 'proper2',
+        name: 'Proper 2',
+        min: 515,
+        max: 521
+    },
+    {
+        id: 'proper3',
+        name: 'Proper 3',
+        min: 522,
+        max: 528
+    },
+    {
+        id: 'proper3',
+        name: 'Proper 3',
+        min: 522,
+        max: 528
+    },
+    {
+        id: 'proper4',
+        name: 'Proper 4',
+        min: 529,
+        max: 604
+    },
+    {
+        id: 'proper5',
+        name: 'Proper 5',
+        min: 605,
+        max: 611
+    },
+    {
+        id: 'proper6',
+        name: 'Proper 6',
+        min: 612,
+        max: 618
+    },
+    {
+        id: 'proper7',
+        name: 'Proper 7',
+        min: 619,
+        max: 625
+    },
+    {
+        id: 'proper8',
+        name: 'Proper 8',
+        min: 626,
+        max: 702
+    },
+    {
+        id: 'proper9',
+        name: 'Proper 9',
+        min: 703,
+        max: 709
+    },
+    {
+        id: 'proper10',
+        name: 'Proper 10',
+        min: 710,
+        max: 716
+    },
+    {
+        id: 'proper11',
+        name: 'Proper 11',
+        min: 717,
+        max: 723
+    },
+    {
+        id: 'proper12',
+        name: 'Proper 12',
+        min: 724,
+        max: 730
+    },
+    {
+        id: 'proper13',
+        name: 'Proper 13',
+        min: 731,
+        max: 806
+    },
+    {
+        id: 'proper14',
+        name: 'Proper 14',
+        min: 807,
+        max: 813
+    },
+    {
+        id: 'proper15',
+        name: 'Proper 15',
+        min: 814,
+        max: 820
+    },
+    {
+        id: 'proper16',
+        name: 'Proper 16',
+        min: 821,
+        max: 827
+    },
+    {
+        id: 'proper17',
+        title: 'Proper 17',
+        min: 828,
+        max: 903
+    },
+    {
+        id: 'proper18',
+        title: 'Proper 18',
+        min: 904,
+        max: 910
+    },
+    {
+        id: 'proper19',
+        title: 'Proper 19',
+        min: 911,
+        max: 917
+    },
+    {
+        id: 'proper20',
+        title: 'Proper 20',
+        min: 918,
+        max: 924
+    },
+    {
+        id: 'proper21',
+        title: 'Proper 21',
+        min: 925,
+        max: 1001
+    },
+    {
+        id: 'proper22',
+        title: 'Proper 22',
+        min: 1002,
+        max: 1008
+    },
+    {
+        id: 'proper23',
+        title: 'Proper 23',
+        min: 1009,
+        max: 1015
+    },
+    {
+        id: 'proper24',
+        title: 'Proper 24',
+        min: 1016,
+        max: 1022
+    },
+    {
+        id: 'proper25',
+        title: 'Proper 25',
+        min: 1023,
+        max: 1029
+    },
+    {
+        id: 'proper26',
+        title: 'Proper 26',
+        min: 1030,
+        max: 1105
+    },
+    {
+        id: 'proper27',
+        title: 'Proper 27',
+        min: 1106,
+        max: 1112
+    },
+    {
+        id: 'proper28',
+        title: 'Proper 28',
+        min: 1113,
+        max: 1119
+    },
+    {
+        id: 'proper29',
+        title: 'Proper 29',
+        min: 1120,
+        max: 1126
+    },
+];
