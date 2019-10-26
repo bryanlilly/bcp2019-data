e6ed58ced1a0f2f48bd514a9f1c700e053da3261
Bryan <lilly.bryan@gmail.com>
2019-10-26 16:59:37 -0400

    feat: add Collects for Advent, Christmas, and Holy Name

---
+++ src/collects/Seasonal.ts
@@ -0,0 +1,38 @@
+export const Seasonal = [
+    {
+        id: 'adventFirstSunday',
+        text: 'Almighty God, give us grace to cast away the works of darkness, and put on the armor of light, now in the time of this mortal life in which your Son Jesus Christ came to visit us in great humility; that in the last day, when we shall come again in his glorious majesty to judge both the living and the dead, we may rise to the life immortal; through him to lives and reigns with you and the Holy Spirit, one God, now and forever.'
+    },
+    {
+        id: 'adventSecondSunday',
+        text: 'Blessed Lord, who cause all Holy Scriptures to be written for our learning; Grant us so to hear them, read, mark, learn, and inwardly digest them, that by patience and the comfort of your holy Word we may embrace and ever hold fast the blessed hope of everlasting life, which you have given us in our Savior Jesus Christ; who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'adventThirdSunday',
+        text: 'O Lord Jesus Christ, you sent your messengers the prophets to preach repentance and prepare the way for our salvation: Grant that the ministers and stewards of your mysteries may likewise make ready your way, by turning the hearts of the disobedient toward the wisdom of the just, that at your second coming to judge the world, we may be found a people acceptable in your sight; for with the Father and the Holy Spirit you live and reign, one God, now and forever.'
+    },
+    {
+        id: 'adventFourthSunday',
+        text: 'Stir up your power, O Lord, and with great might come among us; and a swe are sorely hindered by our sins from running the race that is set before us, let your bountiful grace and mercy speedily help and deliver us; through Jesus Christ our Lord, to whom, with you and the Holy Spirit, be honor and glory, now and for ever.'
+    },
+    {
+        id: 'christmasEve',
+        text: 'O God, you have caused this holy night to shine with the brightness of true Light; Grant that we, who have known the mystery of that Light on earth, may also enjoy him perfectly in heaven; where with you and the Holy Spirit he lives and reigns, one God, in glory everlasting.'
+    },
+    {
+        id: 'christmas',
+        text: 'Almighty God, you have given your only-begotten Son to take our nature upon him, and to be born [this day] of a pure virgin: Grant that we, who have been norn again and made your children by adoption and grace, may daily be renewed by your Holy Spirit; through Jesus Christ our Lord, to whom with you and the same Spirit be honor and glory, now and for ever.'
+    },
+    {
+        id: 'christmasFirstSunday',
+        text: 'Almighty God, you have poured upon us the new light of your incarnate Word: Grant that this light, kindled in our hearts, may shine forth in our lives; through Jesus Christ our Lord, who lives and reigns with you in the unity of the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'christmasSecondSunday',
+        text: 'O God, who wonderfully created, and yet more wonderfully restored, the dignity of human nature: Grant that we may share the diving life of him who humbled himself to share our humanity, your Son Jesus Christ our Lord; who lives and reigns with you, in the unity of the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'circumcisionAndNameOfChrist',
+        text: 'Almighty God, your blessed Son fulfilled the covenant of circumcision for our sake, and was iven the Name that is above every name: Give us grace faithfully to bear his Name, and to worship him with pure hearts according to the New Covenant; who lives and reigns with you, in the unity of the Holy Spirit, one God, now and for ever.'
+    }
+];
