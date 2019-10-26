4154b73ed1f93d52b24f49fc5bd8fea7bddd8171
Bryan <lilly.bryan@gmail.com>
2019-10-26 13:46:10 -0400

    chore: move LaudateDominum

--- src/salutations/index.ts
+++ src/salutations/index.ts
@@ -1,11 +1,13 @@
 import { BenedicamusDomino } from './BenedicamusDomino';
 import { DominusVobiscum } from './DominusVobiscum';
+import { LaudateDominum } from './LaudateDominum';
 import { PaxVobiscum } from './PaxVobiscum';
 import { SursumCorda } from './SursumCorda';
 
 export const Salutations = {
     BenedicamusDomino,
     DominusVobiscum,
+    LaudateDominum,
     PaxVobiscum,
     SursumCorda
 };
--- src/versicles/index.ts
+++ src/versicles/index.ts
@@ -2,7 +2,6 @@ import { DeusInAdiutorium } from './DeusInAdiutorium';
 import { DomineLabiaMeaAperies } from './DomineLabiaMeaAperies';
 import { GloriaPatri, GloriaPatriAlt } from './GloriaPatri';
 import { Kyrie } from './Kyrie';
-import { LaudateDominum } from './LaudateDominum';
 
 export const Versicles = {
     GloriaPatri,
@@ -10,5 +9,4 @@ export const Versicles = {
     Kyrie,
     DeusInAdiutorium,
     DomineLabiaMeaAperies,
-    LaudateDominum,
 };
