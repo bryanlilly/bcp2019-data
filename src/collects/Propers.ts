58a4b992d7ab91d0a69f7f193c918561e4c5c7e5
Bryan <lilly.bryan@gmail.com>
2019-10-26 16:32:53 -0400

    feat: add collects for Proper weeks

---
+++ src/collects/Propers.ts
@@ -0,0 +1,116 @@
+export const Propers = [
+    {
+        id: 'proper1',
+        text: 'O God, the strength of all who put their trust in you: Mercifully accept our prayers, and because, through the weakness of our mortal nature, we can do no good thing without you, grant us the help of your grace to keep your commandments, that we may please you in will and deed; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.',
+    },
+    {
+        id: 'proper2',
+        text: 'O Lord, you never fail to support and govern those whom you bring up in your steadfast love and fear: Keep us, we pray, under your continual protection and providence, and give us a perpetual fear and love of your holy Name; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper3',
+        text: 'O Lord, we entreat you mercifully to hear us, and grant that we, to whom you have given the desire to pray, may by your mighty aid be defended and comforted in all our adversities; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper4',
+        text: 'O God, protector of all those who trust in you, without whom nothing is wrong, nothing is holy; Increase and multiply upon us your mercy, that, with you as our rulter and guide, we may so pass through things temporal that we lose not the things eternal; grant this, heavenly Father, for the sake of your Son Jesus Christ, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper5',
+        text: 'Grant, O Lord, that the course of this world may be so peaceably ordered by your providence, that your Church may joyfully serve you in quiet confidence and godly peace; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper6',
+        text: 'O Lord, from whom all good proceeds: Grant us the inspiration of your Holy Spirit, that we may always think those things that are good, and by your merciful guidance may accomplish the same; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper7',
+        text: 'Lord of all power and might, the author and giver of all good things: Graft in our hearts the love of your Name, increase in us true religion, nourish us with all goodness, and bring forth in us the fruit of good works; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper8',
+        text: 'O God, your never-failing providence sets in order all things both in heaven and on earth: Put away from us all hurtful things, and give us those things that are profitable for us; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper9',
+        text: 'Grant us, O Lord, we pray, the spirit to think and do always those things that are right, that we, who can do no good thing apart from you, may by you be enabled to live according to your will; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper10',
+        text: 'Let your merciful ears, O Lord, be open to the prayers of your humble servants; and, that we may receive what we ask, teach us by your Holy Spirit to ask only those things that are pleasing to you; through Jesus Christ our Lord, who with you and the same Spirit lives and reigns for ever and ever.'
+    },
+    {
+        id: 'proper11',
+        text: 'O God, you declare your almighty power chiefly in showing mercy and pity: Grant us the fullness of your grace, that we, running to obtain your promises, may become partakers of your heavenly treasure; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper12',
+        text: 'Almighty and everlasting God, you are always more ready to hear than we to pray, and to give more than we either desire or observe: Pour down upon us the abundance of your mercy, forgiving us those things of which our conscience is afraid, and giving us those good things for which we are not worthy to ask, except through the merits and mediation of Jesus Christ our Savior; who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper13',
+        text: 'Almighty and merciful God, it is only by your grace that your faithful people offer you true and laudable service: Grant that we may run without stumbling to obtain your heavenly promises; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'proper14',
+        text: 'Almighty God, give us the increase of faith, hope, and love; and, that we may obtain what you have promised, make us love what you command; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper15',
+        text: 'Keep your Church, O Lord, by your perpetual mercy; and because without you the frailty of our nature causes us to fall, keep us from all things hurtful, and lead us to all things profitable for our salvation; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper16',
+        text: 'Let your continual mercy, O Lord, cleanse and defend your Church; and, because it cannot continue in safety without your help, protect and govern it always by your goodness; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper17',
+        text: 'O Lord, we pray that your grace may always precede and follow after us, that we may continually be given to good works; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'proper18',
+        text: 'O Lord God, grant your people grace to withstand the temptations of the world, the flesh, and the devil, and with pure hearts and minds to follow you, the only God; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'proper19',
+        text: 'O God, because without you we are not able to please you, mercifully grant that your Holy Spirit may in all things direct and rule our hearts; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'proper20',
+        text: 'O Lord, you have taught us that without love, all our deeds are worth nothing: Send your Holy Spirit and pour into our hearts that most excellent gift of charity, the true bond of peace and of all virtues, without which whoever lives is counted dead before you; grant this for the sake of your Son Jesus Christ, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper21',
+        text: 'O merciful Lord, grant to your faithful people pardon and peace, that we may be cleansed from all our sins and serve you with a quiet mind; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'proper22',
+        text: 'Keep, O Lord, your household the Church in continual godliness, that through your proection it may be free from all adversities, and devoutly serve you in good works, to the glory of your Name; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'proper23',
+        text: 'Keep, O Lord, your household the Church in continual godliness, that through your proection it may be free from all adversities, and devoutly serve you in good works, to the glory of your Name; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'proper24',
+        text: 'Set us free, loving Father, from the bondage of our sins, and in your goodness and mercy give us the liberty of that abundant life which you have made known to us in our Savior Jesus Christ; who lives and reigns with you, in the unity of the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'proper25',
+        text: 'Almighty and everlasting God, you govern all things both in heaven and on earth: Mercifully hear the supplications of your people, and in our time grant us your peace; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper26',
+        text: 'Grant us, Lord, not to be anxious about earthly things, but to love things heavenly; and even now, as we live among things that are passing away, to hold fast to those that shall endure; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper27',
+        text: 'O God, whose blessed Son came into the world that he might destroy the works of the devil and make us children of God and heirs of eternal life: Grant that, having this hope, we may purify ourselves as he is pure; that, when he comes again with power and great glory, we may be made like him in his eternal and glorious kingdom; where he lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'proper28',
+        text: 'Stir up, O Lord, the wills of your faithful people, that bringing forth in abundance the fruit of good works, they may be abundantly rewarded when our Savior Jesus Christ comes to restore all things; who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    }
+];
+
+// Christ the King: proper28
