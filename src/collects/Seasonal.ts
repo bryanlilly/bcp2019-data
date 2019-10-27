2490588b604a0f6fe283a4e961cef8460ab35846
Bryan <lilly.bryan@gmail.com>
2019-10-27 13:33:09 -0400

    feat: add Epiphany and Lent to Seasonal Collects

--- src/collects/Seasonal.ts
+++ src/collects/Seasonal.ts
@@ -34,5 +34,78 @@ export const Seasonal = [
     {
         id: 'circumcisionAndNameOfChrist',
         text: 'Almighty God, your blessed Son fulfilled the covenant of circumcision for our sake, and was iven the Name that is above every name: Give us grace faithfully to bear his Name, and to worship him with pure hearts according to the New Covenant; who lives and reigns with you, in the unity of the Holy Spirit, one God, now and for ever.'
-    }
+    },
+    {
+        id: 'epiphany',
+        text: 'O God, by the leading of a star you manifested your only Son to the peoples of the earth: Lead us, who know you by faith, to your presence, where we may see your glory face to face; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'epiphanyFirstSunday',
+        text: 'Eternal Father, at the baptism of Jesus you revealed him to be your Son, and your Holy Spirit descended upon him like a dove: Grant that we, who are born again by water and the Spirit, may be faithful as your adopted children; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'epiphanySecondSunday',
+        text: 'Almighty God, whose Son our Savior Jesus Christ is the light of the world: Grant that your people, illumined by your Word and Sacraments, may shine with the radiance of Christ\'s glory, that he may be known, worshiped, and obeyed to the ends of the earth; through Jesus Christ our Lord, who with you and the Holy Spirit lives and reigns, one God, now and for ever.'
+    },
+    {
+        id: 'epiphanyThirdSunday',
+        text: 'Give us grace, O Lord, to answer readily the call of our Savior Jesus Christ and proclaim to all people the Good News of his salvation, that we and the whole world may perceive the glory of his marvelous works; who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'epiphanyFourthSunday',
+        text: 'O God, you know that we are set in the midst of many grave dangers, and because of the frailty of our nature we cannot stand upright: Grant that your strength and protection may support us in all dangers and carry us through every temptation; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'presentation',
+        text: 'Almighty and everliving God, we humbly pray that, as your only-begotten Son was this day presented in the temple in the substance of our flesh, so we may be presented to you with pure and clean hearts by Jesus Christ our Lord; who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'epiphanyFifthSunday',
+        text: 'O Lord, our heavenly Father, keep your household the Church continually in your true religion, that we who trust in the hope of your heavenly grace may always be defended by your mighty power; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, now and for ever.'
+    },
+    {
+        id: 'epiphanySixthSunday',
+        text: 'Almighty God, look mercifully upon your people, that by your great goodness they may be governed and preserved evermore; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, now and for ever.'
+    },
+    {
+        id: 'epiphanySeventhSunday',
+        text: 'O God, the strength of all who put their trust in you: Mercifully accept our prayers, and because, through the weakness of our mortal nature, we can do no good thing without you, grant us the help of your grace, that in keeping your commandments we may please you both in will and deed; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one Godm for ever and ever.'
+    },
+    {
+        id: 'epiphanyEighthSunday',
+        text: 'Almighty and merciful God, in your goddness keep us, we pray, from all things that may hurt us; that we, being ready both in mind and body, may accomplish with joyful hearts those things which belong to your purpose; through Jesus Christ our Lord who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'epiphanySecondToLastSunday',
+        text: 'Almighty God, through the outpouring of the Holy Spirit, you revealed the way of eternal life to every race and nation: Pour out this gift anew, that by the preaching of the Gospel your salvation may reach to the ends of the earth; through Jesus Christ our Lord, who lives and reigns with you, in the unity of the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'epiphanyLastSunday',
+        text: 'O God, who before the passion of your only-begotten Son revealed his glory upon the holy mountain: Grant that we, beholding by faith the light of his countenance, may be strengthened to bear our cross, and be changed into his likeness from glory to glory; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'ashWednesday',
+        text: 'Almighty and everlasting God, you hate nothing you have made, and you forgive the sins of all who are penitent: Create and make in us new and contrite hearts, that we, worthily lamenting our sins and acknowleding our wretchedness, may obtain of you, the God of all mercy, perfect remission and forgiveness; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'lentFirstSunday',
+        text: 'Almighty God, whose blessed Son was led by the Spirit to be tempted by Satan: Come quickly to help us who are assaulted by many temptations, and, as you know the weaknesses of each of us, let each one find you mighty to save; through Jesus Christ your Son our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'lentSecondSunday',
+        text: 'Almighty God, you know that we have no power in ourselves to help ourselves: Keep us both outwardly in our bodies and inwardly in our souls, that we may be defended from all adversities that may happen to the body, and from all evil thoughts that may assault and hurt the soul; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'lentThirdSunday',
+        text: 'Heavenly Father, you have made us for yourself, and our hearts are restless until they rest in you: Look with compassion upon the heartfelt desires of your servants, and purify our disordered affections, that we may behold your eternal glory in the face of Christ Jesus; who lives and reigns with you and the Holy Spirit, one God, for ever and ever.'
+    },
+    {
+        id: 'lentFourthSunday',
+        text: 'Gracious Father, whose blessed Son Jesus Christ came down from heaven to be the true bread which gives life to the world: Evermore give us this bread, that he may live in us, and we in him; who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+    {
+        id: 'lentFifthSunday',
+        text: 'Almighty God, you alone can bring into order the unruly wills and affections of sinners: Grant your people grace to love what you command and desire that you promise; that, among the swift and varied changes of this world, our hearts may surely there be fixed where true joys are to be found; through Jesus Christ our Lord, who lives and reigns with you and the Holy Spirit, one God, now and for ever.'
+    },
+
 ];
